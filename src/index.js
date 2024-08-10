714e4d21e40e42999fce2428f7c927491f5776b8
Xuân Tú <buixuantu01@gmail.com>
2024-08-10 13:29:40 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="nhulac.jpg" alt="Nhu Lac Yeu Dau" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Như Lạc Yêu Dấu</h1>
+      <p>
+        Như Lạc Yêu Dấu, Như Lạc Yêu Dấu, Như Lạc Yêu Dấu,Như Lạc Yêu Dấu, Như
+        Lạc Yêu Dấu, Như Lạc Yêu Dấu, Như Lạc Yêu Dấu,Như Lạc Yêu Dấu
+      </p>
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="🐅" color="red" />
+      <Skill skill="HTML, CSS" emoji="🐛" color="yellow" />
+      <Skill skill="Nodejs" emoji="🐌" color="green" />
+      <Skill skill="RESTful API" emoji="🐬" color="gray" />
+      <Skill skill="Testing software" emoji="🐙" color="orange" />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
